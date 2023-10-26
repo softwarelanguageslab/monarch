1e6b5fac7c5ca38c4a5e5d37dce911205e8ff68e
Bram Vandenbogaerde <bram@vdb.space>
2023-10-26 16:50:57 +0100

    merge from modf branch

--- maf2-domains/src/Domain/Python/CPDomain.hs
+++ maf2-domains/src/Domain/Python/CPDomain.hs
@@ -6,6 +6,7 @@ import Domain.Python.ClassDomain
 import Domain.ConstantPropagation
 import Data.Map
 import Data.Kind
+import Data.Typeable
 
 -- | Constant propagation Python value
 type CPValue adr (a :: Type) =
@@ -13,10 +14,10 @@ type CPValue adr (a :: Type) =
 
 -- | Values bound to variables
 newtype VarAdr adr (a :: Type) = VarAdr adr deriving (Show, Eq, Ord)
-instance (Show adr, Ord adr) => Address (VarAdr adr a) where 
+instance (Show adr, Ord adr, Typeable adr, Typeable a) => Address (VarAdr adr a) where 
    type Vlu (VarAdr adr a) = CPValue adr a
 
 -- | Addresses pointing to objects
 newtype ObjAdr adr (a :: Type) = ObjAdr adr deriving (Show, Eq, Ord)
-instance (Show adr, Ord adr) => Address (ObjAdr adr a) where 
+instance (Show adr, Ord adr, Typeable adr, Typeable a) => Address (ObjAdr adr a) where 
    type Vlu (ObjAdr adr a) = AbstractObject (ObjAdr adr a) (CP String) (CPValue adr a)
--- maf2-domains/src/Domain/Scheme/CPDomain.hs
+++ maf2-domains/src/Domain/Scheme/CPDomain.hs
@@ -5,6 +5,7 @@ import Data.Map
 import Domain.Scheme
 import Domain
 import Domain.ConstantPropagation
+import Data.Dynamic
 
 type CPValue ptr var exp = ModularSchemeValue
                                   (CP Double)
@@ -14,23 +15,25 @@ type CPValue ptr var exp = ModularSchemeValue
                                   (CPPaiAdr ptr var exp)
                                   (CPVecAdr ptr var exp)
                                   (CPStrAdr ptr var exp)
+                                  var
                                   exp
                                   (Map String var)
 
 newtype CPPaiAdr ptr var exp = CPPaiAdr { paiAdr :: ptr } deriving (Show, Eq, Ord, Generic)
 -- instance (Hashable ptr) => Hashable (CPPaiAdr ptr var)
-instance (Show ptr,  Ord ptr, Ord var, Ord exp) => Address (CPPaiAdr ptr var exp) where
+instance (Show ptr,  Ord ptr, Ord var, Ord exp, Typeable ptr, Typeable var, Typeable exp) => Address (CPPaiAdr ptr var exp) where
    type Vlu (CPPaiAdr ptr var exp) = SimplePair (CPValue ptr var exp)
 
 -- instance (Hashable ptr) => Hashable (CPVecAdr ptr var)
 newtype CPVecAdr ptr var exp = CPVecAdr { vecAdr :: ptr } deriving (Show, Eq, Ord, Generic)
-instance (Show ptr, Show var, Ord ptr, Show exp) => Address (CPVecAdr ptr var exp) where
+instance (Show ptr, Show var, Ord ptr, Show exp, Typeable ptr, Typeable var, Typeable exp) => Address (CPVecAdr ptr var exp) where
    type Vlu (CPVecAdr ptr var exp) = PIVector (CPValue ptr var exp) (CPValue ptr var exp)
 
 -- instance (Hashable ptr) => Hashable (CPStrAdr ptr var)
 newtype CPStrAdr ptr var exp = CPStrAdr { strAddr :: ptr } deriving (Show, Eq, Ord, Generic)
-instance (Show ptr, Show var, Ord ptr, Ord var, Ord exp, Show exp) => Address (CPStrAdr ptr var exp) where
+instance (Show ptr, Show var, Ord ptr, Ord var, Ord exp, Show exp, Typeable ptr, Typeable var, Typeable exp) => Address (CPStrAdr ptr var exp) where
    type Vlu (CPStrAdr ptr var exp) = SchemeString (CP String) (CPValue ptr var exp)
+
 
 -- |  The addresses that are expected to be in the store when this type of domain is used
 -- type StoreAdrs ptr var exp = Associate Vlu '[var, CPPaiAdr ptr var exp, CPVecAdr ptr var exp, CPStrAdr ptr var exp]
