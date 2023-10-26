54e88c0ca3932c6b30024df47449c693c6025639
Bram Vandenbogaerde <bram@vdb.space>
2023-10-26 16:33:20 +0100

    moved some typelevel programming from domain to typelevel package

--- maf2-typelevel/src/Data/TypeLevel/List.hs
+++ maf2-typelevel/src/Data/TypeLevel/List.hs
@@ -1,13 +1,11 @@
-{-# LANGUAGE UndecidableInstances, KindSignatures, FlexibleInstances #-}
+{-# LANGUAGE UndecidableInstances, PolyKinds, FlexibleInstances #-}
 {-# OPTIONS_GHC -Wno-redundant-constraints #-}
-{-# OPTIONS_GHC -Wno-unticked-promoted-constructors #-}
 module Data.TypeLevel.List(Has) where
 
-import GHC.TypeLits
-import Data.Kind
+import GHC.TypeError
 
-class Has (l :: [Type]) e 
+class Has (l :: [k]) (v :: k)
+instance {-# OVERLAPPABLE #-} (Has xs x) => Has (y ': xs) x
 instance {-# OVERLAPPING #-} Has (x ': xs) x
-instance Has xs x => Has (y ': xs) x
-instance TypeError (Text "Has: failed to find type " :<>: ShowType x :<>: Text " in typelevel list") => Has '[] x
+instance TypeError (Text "value not in list") => Has '[] v
 
