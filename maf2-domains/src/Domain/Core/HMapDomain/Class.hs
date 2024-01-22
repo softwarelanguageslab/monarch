f80fac47a2114793af3ccc1021741de06e373eeb
Noah Van Es <noahvanes@me.com>
2024-01-22 15:27:06 +0100

    add `from` to HMapDomain

--- maf2-domains/src/Domain/Core/HMapDomain/Class.hs
+++ maf2-domains/src/Domain/Core/HMapDomain/Class.hs
@@ -11,15 +11,18 @@ import Data.TypeLevel.HMap hiding (empty)
 
 import Data.Kind (Type)
 import Data.Singletons
+import Data.Singletons.Sigma
 
 -- | An abstract representation of an HMap
-class (JoinLattice v, ForAll k (AtKey1 JoinLattice m)) => HMapDomain v (m :: [k:->Type]) | v -> m where
+class (JoinLattice v) => HMapDomain v (m :: [k:->Type]) | v -> m where
     empty :: v
     member :: forall (kt :: k) b . (BoolDomain b) => Sing kt -> v -> b
     lookup :: forall kt . Sing kt -> v -> Maybe (Assoc kt m) 
     insert :: forall kt . Sing kt -> Assoc kt m -> v -> v 
     singleton :: forall kt . Sing kt -> Assoc kt m -> v  
     singleton k v = insert k v empty 
+    from :: [BindingFrom m] -> v
+    from = Prelude.foldr (\(s :&: v) -> insert s v) empty
 
 
 
