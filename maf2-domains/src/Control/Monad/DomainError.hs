4365de52294c163fe42ce1199d7fdaf4d81ba4fe
Bram Vandenbogaerde <bram@vdb.space>
2024-02-21 10:21:49 +0100

    domains: factor out escape into Control.Monad.Escape

--- maf2-domains/src/Control/Monad/DomainError.hs
+++ maf2-domains/src/Control/Monad/DomainError.hs
@@ -4,6 +4,8 @@
 -- in the implementation of our abstract domains.
 module Control.Monad.DomainError(DomainError(..), MonadEscape(..), MayEscapeT(..), MayEscape(Value), orElse, try) where
 
+import Control.Monad.Escape
+
 import Lattice hiding (Bottom)
 import Domain.Class 
 
@@ -22,99 +24,5 @@ class Domain a DomainError => ErrorDomain a
 
 instance ErrorDomain (Set DomainError) 
  
--- | Monad to handle errors in the abstract domain
-class MonadEscape m where
-   type Esc m
-   escape :: (JoinLattice a, Domain (Esc m) e) => e -> m a 
-   catch :: JoinLattice a => m a -> (Esc m -> m a) -> m a
-
-orElse :: (MonadEscape m, JoinLattice a) => m a -> m a -> m a
-orElse a = catch a . const  
-
-try :: (MonadEscape m, JoinLattice a) => [m a] -> m a -> m a
-try = flip $ foldr orElse 
-
-------------------------------------------------------------
---- MayEscape
-------------------------------------------------------------
-
-data MayEscape e v = Bottom 
-                   | Escape e 
-                   | Value v
-                   | MayBoth v e
-   deriving (Eq, Ord, Functor)
-
-addError :: Joinable e => e -> MayEscape e a -> MayEscape e a
-addError _ Bottom = Bottom
-addError e1 (Escape e2) = Escape (e1 `join` e2)
-addError e (Value v) = MayBoth v e
-addError e1 (MayBoth v e2) = MayBoth v (e1 `join` e2) 
-
-instance (Joinable e, Joinable a) => Joinable (MayEscape e a) where
-   join Bottom mf                         = mf
-   join mf Bottom                         = mf
-   join (Escape e1) (Escape e2)           = Escape (e1 `join` e2)
-   join (Escape e) (Value v)              = MayBoth v e
-   join (Escape e1) (MayBoth v e2)        = MayBoth v (e1 `join` e2)
-   join (Value v) (Escape e)              = MayBoth v e
-   join (Value v1) (Value v2)             = Value (v1 `join` v2)
-   join (Value v1) (MayBoth v2 e)         = MayBoth (v1 `join` v2) e 
-   join (MayBoth v e1) (Escape e2)        = MayBoth v (e1 `join` e2)
-   join (MayBoth v1 e) (Value v2)         = MayBoth (v1 `join` v2) e
-   join (MayBoth v1 e1) (MayBoth v2 e2)   = MayBoth (v1 `join` v2) (e1 `join` e2)
-
-instance (Eq a, Eq e, Joinable a, Joinable e) => JoinLattice (MayEscape e a) where
-   bottom = Bottom
-
-------------------------------------------------------------
---- MayEscapeT
-------------------------------------------------------------
-
-newtype MayEscapeT m e a = MayEscapeT { runMayEscape :: m (MayEscape e a) }
-
-instance (Monad m) => Functor (MayEscapeT m e) where
-   fmap f (MayEscapeT ma) = MayEscapeT $ fmap (fmap f) ma  
-
-instance (Monad m, Joinable e) => Applicative (MayEscapeT m e) where
-   pure = MayEscapeT . return . Value
-   (<*>) = ap
-
-instance (Monad m, Joinable e) => Monad (MayEscapeT m e) where
-   return = pure
-   MayEscapeT m >>= f = MayEscapeT $ m >>= \case 
-                                             Bottom      -> return Bottom
-                                             Escape e    -> return $ Escape e
-                                             Value v     -> runMayEscape (f v) 
-                                             MayBoth v e -> runMayEscape (f v) <&> addError e
-
-instance (MonadJoin m, Joinable e) => MonadEscape (MayEscapeT m e) where
-   type Esc (MayEscapeT m e) = e 
-   escape = MayEscapeT . return . Escape . inject
-   catch (MayEscapeT m) hdl = MayEscapeT $ m >>= handle
-      where handle Bottom        = return Bottom
-            handle (Escape e)    = runMayEscape (hdl e)
-            handle suc@(Value _) = return suc
-            handle (MayBoth v e) = handle (Value v) `mjoin` handle (Escape e)
-
-instance (Eq e, Joinable e, MonadJoin m) => MonadJoin (MayEscapeT m e) where
-    mzero = MayEscapeT mzero
-    mjoin (MayEscapeT ma) (MayEscapeT mb) = MayEscapeT (mjoin ma mb)
-
-
-------------------------------------------------------------
--- Maybe instance for MonadEscape
-------------------------------------------------------------
-
-instance MonadEscape Maybe where 
-   type Esc Maybe = ()
-   escape = const Nothing
-   -- NOTE: this is an overapproximation,
-   -- we assume that an error can always occur in the `ma` 
-   -- value because `mjoin Nothing (Just a)` = Just a
-   -- and the error information is lost. Therefore
-   -- we always have to pretend that a `Just a` 
-   -- value could also have been an error
-   catch ma f = mjoin ma (f ())
-
 instance Domain () DomainError where
    inject = const ()
