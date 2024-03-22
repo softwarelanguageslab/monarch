e94f471c97e1d4d95248f69297293c067caedcbd
Noah Van Es <noahvanes@me.com>
2024-03-22 15:58:07 +0100

    add Python object domain to maf-domains

---
+++ maf2-domains/src/Domain/Python/Objects/PyObjHMap.hs
@@ -0,0 +1,113 @@
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE ConstraintKinds #-}
+
+module Domain.Python.Objects.PyObjHMap where
+
+import Lattice
+import qualified Domain 
+import Domain.Core
+import Domain.Core.SeqDomain
+import Domain.Python.World 
+import Domain.Python.Objects.Class 
+
+import Domain.Core.HMapDomain (HMapAbs(..))
+import qualified Domain.Core.HMapDomain as HMapDomain 
+import Data.TypeLevel.HMap 
+import qualified Data.TypeLevel.HMap as HMap  
+
+import Data.Set (Set)
+import Data.Map (Map)
+import Data.Kind 
+import Data.Maybe
+import Data.Singletons (fromSing)
+import Data.Singletons.Sigma (Sigma((:&:)))
+
+--
+-- Configuring the abstractions 
+--
+
+data PyAbsKey = IntKey
+              | ReaKey  
+              | BlnKey
+              | StrKey
+
+type PyPrm (m :: [PyAbsKey :-> Type]) vlu adr clo =
+  '[
+    IntPrm ::-> Assoc IntKey m,
+    ReaPrm ::-> Assoc ReaKey m,
+    BlnPrm ::-> Assoc BlnKey m,
+    StrPrm ::-> Assoc StrKey m,
+    PrmPrm ::-> Set PyPrim,
+    CloPrm ::-> Set clo,
+    BndPrm ::-> Map adr vlu,    -- alternative, but less precise: (PyVal, PyVal)
+    TupPrm ::-> CPList vlu,     -- TODO: could use a more optimised representation (e.g., CPVector)
+    LstPrm ::-> CPList vlu,
+    NonPrm ::-> ()
+  ]
+
+--
+-- The PyObjHMap type
+--
+
+data PyObjHMap (m :: [PyAbsKey :-> Type]) vlu adr clo = PyObjHMap { dct :: CPDictionary String vlu,
+                                                                    prm :: HMapAbs (PyPrm m vlu adr clo) }
+
+type AllJoin m vlu adr clo = (ForAll PyPrmKey (AtKey1 JoinLattice (PyPrm m vlu adr clo)),
+                              ForAll PyPrmKey (AtKey1 Joinable (PyPrm m vlu adr clo)),
+                              ForAll PyPrmKey (AtKey1 Eq (PyPrm m vlu adr clo)))
+type AllAbs m vlu adr clo = (AllJoin m vlu adr clo,
+                             JoinLattice vlu,
+                             BoolDomain   (Assoc BlnKey m),
+                             IntDomain    (Assoc IntKey m),
+                             Domain.Rea   (Assoc IntKey m) ~ Assoc ReaKey m,
+                             Domain.Str   (Assoc IntKey m) ~ Assoc StrKey m,
+                             Domain.Boo   (Assoc IntKey m) ~ Assoc BlnKey m,
+                             RealDomain   (Assoc ReaKey m),
+                             Domain.Boo   (Assoc ReaKey m) ~ Assoc BlnKey m,
+                             StringDomain (Assoc StrKey m))
+
+deriving instance (JoinLattice vlu, ForAll PyPrmKey (AtKey1 Eq       (PyPrm m vlu adr clo))) => Eq (PyObjHMap m vlu adr clo)
+instance          (JoinLattice vlu, ForAll PyPrmKey (AtKey1 Joinable (PyPrm m vlu adr clo))) => Joinable (PyObjHMap m vlu adr clo) where
+  join (PyObjHMap d1 p1) (PyObjHMap d2 p2) = PyObjHMap (join d1 d2) (join p1 p2)
+instance (JoinLattice vlu, AllJoin m vlu adr clo) => JoinLattice (PyObjHMap m vlu adr clo) where
+  bottom = PyObjHMap bottom bottom
+
+instance (Show vlu, ForAll PyPrmKey (AtKey1 Show (PyPrm m vlu adr clo))) => Show (PyObjHMap m vlu adr clo) where
+  show (PyObjHMap dct (HMapAbs (prm, _))) = show primitives ++ "(" ++ attributes ++ ")" 
+    where attributes = show dct
+          primitives = Prelude.map showPrm (HMap.toList prm)
+          showPrm :: BindingFrom (PyPrm m vlu adr clo) -> (String, String) 
+          showPrm (key :&: vlu) = withC_ @(AtKey1 Show (PyPrm m vlu adr clo)) (show $ fromSing key, show vlu) key
+
+--
+-- Making an instance of PyObj 
+--
+
+instance (Ord clo, Ord adr, AllAbs m vlu adr clo) => PyObj (PyObjHMap m vlu adr clo) where
+    type Ref (PyObjHMap m vlu adr clo) = vlu 
+    type Adr (PyObjHMap m vlu adr clo) = adr
+    type Clo (PyObjHMap m vlu adr clo) = clo 
+    -- creation
+    new cls = PyObjHMap dct prm
+        where dct = Domain.from [(Constant (attrStr ClassAttr), cls)]
+              prm = HMapDomain.empty 
+    -- primitives
+    type Abs (PyObjHMap m vlu adr clo) k = Assoc k (PyPrm m vlu adr clo)
+    hasPrm k = HMapDomain.member k . prm 
+    getPrm k = fromJust . HMapDomain.lookup k . prm 
+    setPrm k v (PyObjHMap dct prm) = PyObjHMap dct $ HMapDomain.insert k v prm 
+    -- attributes
+    hasAttr k = Domain.contains (Constant k) . dct
+    getAttr k = Domain.lookup (Constant k) . dct 
+    setAttr k v (PyObjHMap dct prm) = PyObjHMap (Domain.update (Constant k) v dct) prm 
+    setAttrWeak k v (PyObjHMap dct prm) = PyObjHMap (Domain.updateWeak (Constant k) v dct) prm 
+
+--
+-- A common instantiation
+--
+
+type PyObjCP = PyObjHMap '[IntKey ::-> CP Integer,
+                           ReaKey ::-> CP Double,  
+                           BlnKey ::-> CP Bool,
+                           StrKey ::-> CP String]
---
+++ maf2-domains/src/Domain/Python/World.hs
@@ -0,0 +1,199 @@
+{-# LANGUAGE UndecidableInstances #-}
+
+module Domain.Python.World where
+
+import Data.Finite
+import Data.TypeLevel.HMap (All, ForAllOf, ForAll(..), Dict(..), genHKeys)
+
+import Prelude hiding (True, False, all)
+import Data.Singletons (Sing)
+
+
+-- | Built-in types in Python
+data PyType = NoneType
+            | BoolType
+            | IntType
+            | StringType
+            | TupleType
+            | PrimType
+            | BoundType
+            | CloType
+            | FloatType
+            | ListType 
+  deriving (Eq, Ord, Enum, Bounded, Show) 
+
+-- | The name of a built-in Python type 
+name :: PyType -> String
+name NoneType   = "NoneType"
+name IntType    = "int"
+name FloatType  = "float"
+name BoolType   = "bool"
+name StringType = "str"
+name TupleType  = "tuple"
+name PrimType   = "primitive"
+name CloType    = "function"
+name BoundType  = "bound function" 
+name ListType   = "list"
+
+-- | The methods of a built-in Python type 
+methods :: PyType -> [(PyAttr, PyPrim)]
+methods IntType = [(AddAttr,      IntAdd),
+                   (SubAttr,      IntSub),
+                   (MulAttr,      IntMul),
+                   (TrueDivAttr,  IntTrueDiv),
+                   (EqAttr,       IntEq),
+                   (NeAttr,       IntNe),
+                   (LtAttr,       IntLt),
+                   (GtAttr,       IntGt),
+                   (LeAttr,       IntLe),
+                   (GeAttr,       IntGe)]
+methods FloatType = [(AddAttr,      FloatAdd),
+                     (SubAttr,      FloatSub),
+                     (MulAttr,      FloatMul),
+                     (TrueDivAttr,  FloatTrueDiv),
+                     (EqAttr,       FloatEq),
+                     (NeAttr,       FloatNe),
+                     (LtAttr,       FloatLt),
+                     (GtAttr,       FloatGt),
+                     (LeAttr,       FloatLe),
+                     (GeAttr,       FloatGe)] 
+methods NoneType    = []
+methods BoolType    = []
+methods StringType  = []
+methods TupleType   = [] 
+methods PrimType    = []
+methods BoundType   = []
+methods CloType     = []
+methods ListType    = []
+
+-- | Built-in primitives in Python
+data PyPrim     = 
+                -- integer primitives
+                  IntAdd
+                | IntMul
+                | IntSub
+                | IntTrueDiv
+                | IntEq
+                | IntNe
+                | IntLt
+                | IntGt
+                | IntLe
+                | IntGe
+                -- float primitives
+                | FloatAdd
+                | FloatSub
+                | FloatMul
+                | FloatTrueDiv
+                | FloatEq
+                | FloatNe
+                | FloatLt
+                | FloatGt
+                | FloatLe
+                | FloatGe
+  deriving (Eq, Ord, Enum, Bounded, Show)
+
+-- | Built-in attributes in Python
+data PyAttr = ClassAttr 
+            | AddAttr 
+            | MulAttr 
+            | PowAttr
+            | LtAttr
+            | GtAttr  
+            | EqAttr 
+            | GeAttr 
+            | LeAttr
+            | NeAttr 
+            | OrAttr 
+            | XorAttr 
+            | AndAttr 
+            | LShiftAttr
+            | RShiftAttr 
+            | SubAttr
+            | TrueDivAttr
+            | FloorDivAttr 
+            | MatMulAttr 
+            | InvertAttr
+            | ModAttr 
+            | NameAttr
+            | MROAttr 
+  deriving (Eq, Ord, Enum, Bounded)
+
+attrStr :: PyAttr -> String 
+attrStr ClassAttr     = "__class__"
+attrStr AddAttr       = "__add__"
+attrStr SubAttr       = "__sub__"
+attrStr MulAttr       = "__mul__"
+attrStr TrueDivAttr   = "__truediv__"
+attrStr FloorDivAttr  = "__floordiv__"
+attrStr PowAttr       = "__pow__"
+attrStr MatMulAttr    = "__matmul__"
+attrStr InvertAttr    = "__invert__"
+attrStr ModAttr       = "__mod__"
+attrStr LtAttr        = "__lt__"
+attrStr GtAttr        = "__gt__"
+attrStr EqAttr        = "__eq__"
+attrStr GeAttr        = "__ge__"
+attrStr LeAttr        = "__le__"
+attrStr NeAttr        = "__ne__"
+attrStr OrAttr        = "__or__"
+attrStr XorAttr       = "__xor__"
+attrStr AndAttr       = "__and__"
+attrStr LShiftAttr    = "__lshift__"
+attrStr RShiftAttr    = "__rshift__"
+attrStr NameAttr      = "__name__"
+attrStr MROAttr       = "__mro__"
+
+-- | Built-in objects in Python 
+data PyConstant = Type      -- 'type'
+                | Object    -- 'object'
+                | None
+                | True
+                | False
+                | TypeObject PyType 
+                | TypeName   PyType 
+                | TypeMRO    PyType 
+                | PrimObject PyPrim 
+  deriving (Eq, Ord, Show)
+
+instance Finite PyConstant where
+  all = [Type, Object, None, True, False] 
+        ++ map TypeObject all 
+        ++ map TypeName   all 
+        ++ map TypeMRO    all
+        ++ map PrimObject all
+
+-- | Built-in primitive fields in Python
+data PyPrmKey = IntPrm
+              | ReaPrm 
+              | BlnPrm
+              | StrPrm
+              | PrmPrm
+              | CloPrm
+              | BndPrm
+              | TupPrm
+              | LstPrm
+              | NonPrm 
+  deriving (Eq, Ord, Show)
+
+genHKeys ''PyPrmKey
+
+classFor :: SPyPrmKey k -> PyType
+classFor SIntPrm = IntType 
+classFor SReaPrm = FloatType
+classFor SBlnPrm = BoolType
+classFor SStrPrm = StringType
+classFor SPrmPrm = PrimType
+classFor SCloPrm = CloType
+classFor SBndPrm = BoundType
+classFor STupPrm = TupleType
+classFor SLstPrm = ListType 
+classFor SNonPrm = NoneType 
+
+-- | Built-in Python errors
+data PyError = AttributeNotFound
+             | NotMutable
+             | NotCallable
+             | ArityError
+             | TypeMismatch
+             | InvalidMRO
+  deriving (Eq, Ord)
