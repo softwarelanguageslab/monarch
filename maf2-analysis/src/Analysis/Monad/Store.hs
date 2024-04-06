0248405058bb17910f93f204a74a6dc0410d8e7e
Noah Van Es <noahvanes@me.com>
2024-04-06 11:23:37 +0200

    add convenience functions to check if the store has changed

--- maf2-analysis/src/Analysis/Monad/Store.hs
+++ maf2-analysis/src/Analysis/Monad/Store.hs
@@ -2,7 +2,7 @@
 {-# LANGUAGE UndecidableInstances #-}
 
 module Analysis.Monad.Store (
-    StoreM(..), 
+    StoreM(..),
     StoreT,
     StoreT',
     runStoreT,
@@ -11,12 +11,15 @@ module Analysis.Monad.Store (
     deref,
     deref',
     store,
-) 
+    writeAdr',
+    updateAdr',
+    updateWith',
+)
 where
 
 import Lattice (Joinable(..), JoinLattice(..))
-import qualified Analysis.Store as Store 
-import Analysis.Monad.Allocation 
+import qualified Analysis.Store as Store
+import Analysis.Monad.Allocation
 
 import Control.Monad.State.SVar (SVar)
 import qualified Control.Monad.State.SVar as SVar
@@ -24,7 +27,7 @@ import qualified Control.Monad.State.SVar as SVar
 import Data.Map (Map)
 import qualified Data.Map as Map
 import Data.Set (Set)
-import qualified Data.Set as Set 
+import qualified Data.Set as Set
 import Control.Monad.State hiding (mzero, join)
 import Control.Monad.Join (MonadJoin(..), mjoinMap, mjoins)
 
@@ -50,6 +53,23 @@ class (Monad m, Joinable v) => StoreM m a v | m a -> v where
 
    {-# MINIMAL lookupAdr, writeAdr, (updateAdr | updateWith) #-}
 
+updateAndCheck :: (StoreM m a v, Eq v) => a -> (a -> m ()) -> m Bool
+updateAndCheck a f = do old <- lookupAdr a
+                        f a
+                        new <- lookupAdr a
+                        return (old == new)
+
+-- | Convenience function: writes to an address `a` and checks if the value in the store at `a` has changed
+writeAdr' :: (StoreM m a v, Eq v) => a -> v -> m Bool
+writeAdr' a v = updateAndCheck a (`writeAdr` v)
+
+-- | Convenience function: updates an address `a` and checks if the value in the store at `a` has changed
+updateAdr' :: (StoreM m a v, Eq v) => a -> v -> m Bool
+updateAdr' a v = updateAndCheck a (`updateAdr` v)
+
+-- | Convenience function: updates an address `a` and checks if the value in the store at `a` has changed
+updateWith' :: (StoreM m a v, Eq v) => {- strong update -} (v -> v) -> {- weak update -} (v -> v) -> a -> m Bool
+updateWith' fs fw a = updateAndCheck a (updateWith fs fw)
 
 -- | Lookup
 lookups :: (JoinLattice a, MonadJoin m) => (adr -> m v) -> (adr -> v -> m a) -> Set adr -> m a
@@ -59,7 +79,7 @@ lookups look f = mjoinMap (\adr -> look adr >>= f adr)
 deref :: (StoreM m a v, MonadJoin m, JoinLattice r) => (a -> v -> m r) -> Set a -> m r
 deref = lookups lookupAdr
 
-deref' :: (StoreM m a v, MonadJoin m, JoinLattice v) => Set a -> m v 
+deref' :: (StoreM m a v, MonadJoin m, JoinLattice v) => Set a -> m v
 deref' = mjoins . map lookupAdr . Set.toList
 
 -- | Store the given value in the store using the an address allocator on the monadic stack.
