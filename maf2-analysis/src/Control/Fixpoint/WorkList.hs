673f608be1934a20570e66b6cb725fa0c0d68461
Noah Van Es <noahvanes@me.com>
2024-04-08 23:32:24 +0200

    extend `WorkList` typeclass

--- maf2-analysis/src/Control/Fixpoint/WorkList.hs
+++ maf2-analysis/src/Control/Fixpoint/WorkList.hs
@@ -8,10 +8,11 @@ import Data.List (uncons)
 
 -- | WorkList typeclass
 class WorkList wl e | wl -> e where
+   empty :: wl 
    isEmpty :: wl -> Bool
    isEmpty = isNothing . peek 
    add :: e -> wl -> wl
-   addAll :: [e] -> wl -> wl
+   addAll :: Foldable t => t e -> wl -> wl
    addAll = flip (foldr add)
    pop :: wl -> Maybe (e, wl)
    peek :: wl -> Maybe e
@@ -20,12 +21,14 @@ class WorkList wl e | wl -> e where
 -- | LIFO worklist using a list 
 -- | Not recommended due to duplicates
 instance WorkList [a] a where
+   empty = []
    add = (:)
    pop = uncons    
 
 -- | Arbitrary worklist algorithm using a Set 
 -- | Ensures no duplicates are added to the worklist
 instance Ord a => WorkList (Set a) a where 
+   empty = Set.empty
    add = Set.insert 
    pop s
       | Set.null s = Nothing
