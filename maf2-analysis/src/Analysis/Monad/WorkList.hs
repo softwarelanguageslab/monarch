ca8cd968ffab6ef97cf2b6f1f622e4e664f7518c
Noah Van Es <noahvanes@me.com>
2024-04-08 23:28:03 +0200

    add `WorkList` monad transformer

---
+++ maf2-analysis/src/Analysis/Monad/WorkList.hs
@@ -0,0 +1,54 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE UndecidableInstances #-}
+
+module Analysis.Monad.WorkList (
+    WorkListM(..),
+    WorkListT,
+    runWithWorkList,
+    iterateWL,
+) where
+
+import Control.Fixpoint.WorkList (WorkList)
+import qualified Control.Fixpoint.WorkList as WL
+import Control.Monad.Layer
+
+import Control.Monad.State
+import Data.Maybe (fromMaybe)
+import Control.Monad.Cond (unlessM)
+import Data.Functor (($>))
+
+
+---
+--- WorkListM typeclass
+---
+
+class Monad m => WorkListM m cmp | m -> cmp where
+    done :: m Bool
+    pop  :: m cmp
+    add  :: cmp -> m ()
+    adds :: Foldable t => t cmp -> m ()
+    adds = mapM_ add
+
+
+---
+--- WorkListT monad layer 
+---
+
+newtype WorkListT w m a = WorkListT (StateT w m a)
+    deriving (Functor, Applicative, Monad, MonadTrans, MonadLayer, MonadState w)
+
+instance {-# OVERLAPPING #-} (Monad m, WorkList w cmp) => WorkListM (WorkListT w m) cmp where
+    done = gets WL.isEmpty
+    pop = gets (fromMaybe (error "worklist is too empty") . WL.pop) >>= \(cmp, rst) -> put rst $> cmp
+    add = modify . WL.add
+
+instance (MonadLayer t, WorkListM m cmp) => WorkListM (t m) cmp where
+    done = upperM done
+    pop = upperM pop
+    add = upperM . add
+
+runWithWorkList :: forall w cmp m a . (Monad m, WorkList w cmp) => WorkListT w m a -> m a
+runWithWorkList (WorkListT m) = fst <$> runStateT m WL.empty 
+
+iterateWL :: WorkListM m cmp => (cmp -> m ()) -> m ()
+iterateWL f = unlessM done (pop >>= f >> iterateWL f)
