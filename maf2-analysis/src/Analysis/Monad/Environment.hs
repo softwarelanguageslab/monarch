2b9ca07d7d9fdf3eb0e293e4874579ec2adfb440
Bram Vandenbogaerde <bram@vdb.space>
2024-08-07 15:22:32 +0200

    default implementations for methods in EnvM

--- maf2-analysis/src/Analysis/Monad/Environment.hs
+++ maf2-analysis/src/Analysis/Monad/Environment.hs
@@ -9,9 +9,9 @@ module Analysis.Monad.Environment (
 
 import Analysis.Environment
 
-import Control.Monad.Reader 
-import Control.Monad.Join 
-import Control.Monad.Layer 
+import Control.Monad.Reader
+import Control.Monad.Join
+import Control.Monad.Layer
 import Analysis.Monad.Cache (MonadCache)
 
 ---
@@ -24,20 +24,23 @@ class (Monad m, Environment env adr) => EnvM m adr env | m -> env, m -> adr wher
    -- may throw an exception if the identifier is not found 
    -- since it means that the program is not well-formed.
    lookupEnv :: String -> m adr
+   lookupEnv nam = fmap (Analysis.Environment.lookup nam) getEnv
    -- | Extend the environment with the given list of bindings and run the computation
    -- given as an argument in it
    withExtendedEnv :: [(String, adr)] -> m a -> m a
+   withExtendedEnv bds = withEnv (extends bds)
    -- | Retrieves the current environment 
    getEnv :: m env
    -- | Runs function `f` on the environment and to compute the environment to execute `m` in
    withEnv :: {- f -} (env -> env) -> m a -> m a
    -- | Runs `m` in the context of the value produced by `(ctx -> ctx)`.
+   {-# MINIMAL withEnv, getEnv #-}
 
 ---
 --- The EnvT monad transformer
 ---
 
-newtype EnvT env m a = EnvT (ReaderT env m a) 
+newtype EnvT env m a = EnvT (ReaderT env m a)
    deriving (MonadReader env, MonadJoin, Monad, Applicative, MonadLayer, MonadTrans, Functor, MonadCache)
 
 instance {-# OVERLAPPING #-} (Environment env adr, Monad m) => EnvM (EnvT env m) adr env where
