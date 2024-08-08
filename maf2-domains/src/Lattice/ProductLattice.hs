111e2663acaa403f46bd1507b3b96839b8dc9633
Bram Vandenbogaerde <bram@vdb.space>
2024-08-08 15:06:09 +0200

    feat(domain): implement `Meetable` and `EqualLattice` for pairs of values

--- maf2-domains/src/Lattice/ProductLattice.hs
+++ maf2-domains/src/Lattice/ProductLattice.hs
@@ -3,14 +3,26 @@
 
 module Lattice.ProductLattice where
 
+import Prelude hiding (and)
+
 import Lattice.Class
 import Lattice.Split
 
 import qualified Data.Set as Set 
+import Lattice.Equal (EqualLattice(..))
+import Domain.Core.BoolDomain.Class
     
 -- | Joinable for pairs of values
 instance (Joinable v, Joinable w) => Joinable (v, w) where
    join (v1, w1) (v2, w2) = (join v1 v2, join w1 w2)
+
+-- | Meetable for pairs of values
+instance (Meetable v, Meetable w) => Meetable (v, w) where  
+   meet (v1, w1) (v2, w2) = (meet v1 v2, meet w1 w2)
+   
+-- | EqualLattice for pairs of values
+instance (EqualLattice v, EqualLattice w) => EqualLattice (v, w) where  
+   eql (v1, w1) (v2, w2) = and (eql v1 v2) (eql w1 w2)
 
 instance (BottomLattice v, BottomLattice w) => BottomLattice (v, w) where  
    bottom = (bottom, bottom)
