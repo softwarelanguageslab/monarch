1dec08ba333d758485875f8c161ef7385f32890b
Bram Vandenbogaerde <bram@vdb.space>
2024-08-08 13:42:07 +0200

    re-export singletons

--- maf2-domains/src/Domain/Scheme/Modular.hs
+++ maf2-domains/src/Domain/Scheme/Modular.hs
@@ -194,6 +194,7 @@ deriving instance (HMapKey (Values m), ForAll SchemeKey (AtKey1 Joinable (Values
 deriving instance (HMapKey (Values m), ForAll SchemeKey (AtKey1 BottomLattice (Values m))) => BottomLattice (SchemeVal m)
 
 -- Show instance
+-- TODO: this should be valid for any HMap, maybe make it the default implementation?
 instance (ForAll SchemeKey (AtKey1 Show (Values m))) => Show (SchemeVal m) where
    show (SchemeVal hm) = intercalate "," $ map showElement $ HMap.toList hm
       where showElement :: BindingFrom (Values m) -> String
