a41b4b88e037937c97243ee1c230dab79e22f1e4
Bram Vandenbogaerde <bram@vdb.space>
2024-09-11 11:31:08 +0200

    feat: add meta annotation to cc combinator compiler

--- racket/translations/cc-combinator.rkt
+++ racket/translations/cc-combinator.rkt
@@ -2,6 +2,16 @@
 
 (provide translate)
 
+;; Indicates whether to compile meta annotations in the 
+;; output
+(define *is-meta* (make-parameter #f))
+
+;; Generate a meta instruction if the `*is-meta* parameter is #t,
+(define (instrument-meta e)
+  (if (*is-meta*)
+   `(meta ,e)
+   e))
+
 ;; "Contract-combinators" translation to
 ;; regular λ-calculus.
 ;;
@@ -10,21 +20,33 @@
 ;; not always directly appear in contract 
 ;; monitors before program reduction.
 
-(define (translate e)
+
+(define (translate-aux e) 
   (match e
     [(quasiquote (flat ,e))
      (let ((j (gensym)) (k (gensym)) (v (gensym)))
-     `(lambda (,j ,k ,v) (if (,e ,v) ,v (blame ,j (quote ,e)))))] 
+        (instrument-meta `(lambda (,j ,k ,v) (if (,e ,v) ,v (blame ,j (quote ,e))))))] 
     [(quasiquote (-> ,κ1 ,κ2))
      (let ((j (gensym)) (k (gensym)) (f (gensym)) (v (gensym)))
-       `(lambda (,j ,k ,f) 
-         (lambda (,v)
-           (,(translate κ2) ,j ,k (,f (,(translate κ1) ,k ,j ,v))))))]
+       (instrument-meta 
+         `(lambda (,j ,k ,f) 
+            (lambda (,v)
+              (,(translate-aux κ2) ,j ,k (,f (,(translate-aux κ1) ,k ,j ,v)))))))]
     [(quasiquote (mon ,j ,k ,κ ,v))
-     `(,(translate κ) (quote ,j) (quote ,k) ,(translate v))]
+     (instrument-meta `(,(translate-aux κ) (quote ,j) (quote ,k) ,(translate-aux v)))]
     [(quasiquote (,es ...))
-     `(,@(map translate es))]
+     `(,@(map translate-aux es))]
     [x x]))
+
+(define (preluded e)
+  `(letrec 
+     ((any? (lambda (v) #t))
+      (meta (lambda (v) v)))
+     ,e))
+
+(define (translate e #:meta [meta #f])
+  (parameterize ([*is-meta* meta])
+     (preluded (translate-aux e))))
 
 (module+ main
   (require "../tests/prelude.rkt")
@@ -37,8 +59,8 @@
    ;(displayln (eval_ `(,(translate test-3) (lambda (x) (* x 2))))) ;; returns 84
    ;(displayln (eval_ `(,(translate test-3) (lambda (x) (* x -2))))) ;; return blame error on k
 
-
-   (displayln (eval_ (translate test-4))) ;; returns 5
+   (displayln (translate test-4 #:meta #t))
+   (displayln (eval_ (translate test-4 #:meta #t))) ;; returns 5
    (displayln (eval_ `(,(translate test-5) 2))) ;; returns 1
    (displayln (eval_ `(,(translate test-5) 1))) ;; blame error blaming k
 
