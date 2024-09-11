598e10052c244204ef2b334a0160f27ab728eb19
Bram Vandenbogaerde <bram@vdb.space>
2024-09-11 17:25:46 +0200

    refactor(actors): paramatrize actor domain with the type of expression

--- maf2-domains/src/Domain/Scheme/Actors/CP.hs
+++ maf2-domains/src/Domain/Scheme/Actors/CP.hs
@@ -6,35 +6,31 @@ import Data.TypeLevel.HMap ((::->))
 import Data.Map (Map)
 import Lattice
 import Domain.Scheme.Class hiding (Exp)
+import Domain.Scheme.Actors.Class
 import Domain.Core
 
-type CPActorMapping adr pai vec str ctx = 
+type CPActorMapping adr pai vec str ctx exp =
     '[ RealConf ::-> CP Double,
        IntConf ::-> CP Integer,
        CharConf ::-> CP Char,
        BoolConf ::-> CP Bool,
-       EnvConf ::-> Map String adr,
-       ExpConf ::-> Exp,
-       StrConf ::-> str,
-       PaiConf ::-> pai,
-       VecConf ::-> vec,
-       VarConf ::-> adr,
-       PidConf ::-> Pid ctx
+       EnvConf ::-> Map String (adr ctx),
+       ExpConf ::-> exp,
+       StrConf ::-> str ctx,
+       PaiConf ::-> pai ctx,
+       VecConf ::-> vec ctx,
+       VarConf ::-> adr ctx,
+       PidConf ::-> Pid exp ctx
      ]
 
 -- | Type of values in the analysis
-type CPActorValue adr pai vec str ctx =
-  SchemeVal (CPActorMapping adr pai vec str ctx)
+type CPActorValue adr pai vec str ctx exp =
+  SchemeVal (CPActorMapping adr pai vec str ctx exp)
 
 
-data Pid ctx
-  = Pid Exp ctx
-  | EntryPid
-  deriving (Ord, Eq, Show)
+type instance VarDom (CPActorValue adr pai vec str ctx exp) = CPActorValue adr pai vec str ctx exp
+type instance PaiDom (CPActorValue adr pai vec str ctx exp) = SimplePair (CPActorValue adr pai vec str ctx exp)
+type instance VecDom (CPActorValue adr pai vec str ctx exp) = PIVector (CPActorValue adr pai vec str ctx exp) (CPActorValue adr pai vec str ctx exp) 
+type instance StrDom (CPActorValue adr pai vec str ctx exp) = SchemeString (CP String) (CPActorValue adr pai vec str ctx exp) 
 
-type instance VarDom (CPActorValue adr pai vec str ctx) = CPActorValue adr pai vec str ctx
-type instance PaiDom (CPActorValue adr pai vec str ctx) = SimplePair (CPActorValue adr pai vec str ctx)
-type instance VecDom (CPActorValue adr pai vec str ctx) = PIVector (CPActorValue adr pai vec str ctx) (CPActorValue adr pai vec str ctx)
-type instance StrDom (CPActorValue adr pai vec str ctx) = SchemeString (CP String) (CPActorValue adr pai vec str ctx)
-
-instance (Ord ctx, Show ctx) => Address (Pid ctx)
+instance (Ord ctx, Show ctx, Ord exp, Show exp) => Address (Pid exp ctx)
