4ca344d4de6d21936209e6f7d7cd229c29b077a0
Bram Vandenbogaerde <bram@vdb.space>
2024-10-01 23:27:02 +0200

    docs(example): updated documentation for the lambda language example

--- examples/maf2-lambda/README.md
+++ examples/maf2-lambda/README.md
@@ -1,1 +1,36 @@
-# maf2-lambda
+# Lambda Calculus Example
+
+This directory contains a `cabal` project which 
+uses Monarch as a library for implementing 
+a static analysis for a variant of the lambda calculus.
+
+Its structure is similar to the analyses defined in the 
+main Monarch packages (i.e., `maf2-analysis`). It 
+is structured as folows:
+
+* `Domain.Lambda` contains the definition of the abstract 
+domain and an instantiation of this abstract domain 
+using a sparse labeled product
+* `Syntax.Lambda` contains the abstract syntax tree
+ for the language 
+* `Analysis.Lambda` contains both the semantics 
+of the language as well as an instantiation of the 
+analysis which computes an approximation of 
+the programs memory and its results.
+
+## Building and Running the Project
+
+The project can be compiled and executed using `cabal`:
+
+```
+$ cabal build
+$ cabal run
+```
+
+The entrypoint of this example language is configured
+ to analyze the result of the following program, expressed
+below in S-expression syntax:  
+
+```scheme
+((lambda (x) (+ x 1) 0)
+```
--- examples/maf2-lambda/src/Analysis/Lambda.hs
+++ examples/maf2-lambda/src/Analysis/Lambda.hs
@@ -27,24 +27,48 @@ import Domain.Class (Domain)
 import Control.Monad.DomainError (DomainError)
 import Data.Set (Set)
 
+------------------------------------------------------------
+-- Semantics
+------------------------------------------------------------
 
+-- | Addresses are represented by their name
 newtype Adr = Adr String
             deriving (Eq, Show, Ord)
 
+-- | The set of constraints on the monadic 
+-- context used in the analysis. 
+--
+-- This makes a number of functions available
+-- for expressing the program semantics.
 type LamM m = (Monad m,
-               MonadJoin m,
-               StoreM m Adr V,
-               MonadFixpoint m Exp V,
-               MonadEscape m,
+               MonadJoin m,           -- approximating program branches
+               StoreM m Adr V,        -- storing values of variables
+               MonadFixpoint m Exp V, -- fixpoints
+               MonadEscape m,         -- escape/exceptions
                Domain (Esc m) DomainError,
-               EnvM m Adr (Env Adr))
+               EnvM m Adr (Env Adr) -- lexical environment
+            )
 
+-- | We fix the type of values here to `V` from @Domain.Lambda$.
+--
+-- In practice, however, you would make the abstract domain 
+-- polymorphic and have type class constraints expressing
+-- what operations the domain should support.
 type V = LamVal Adr
 type Sto = Map Adr (SVar.SVar V)
 
+-- | The evaluation function
 eval :: LamM m => Exp -> m V 
-eval = fix eval'
-
+eval = fix eval' -- we use `fix` here so that 
+                 -- the open recursion in `eval'` 
+                 -- can use `recur` to schedule
+                 -- the recursive call in the 
+                 -- fixpoint algorithm
+                 
+-- | The real evaluation function,
+-- it is mostly straightforward as 
+-- it is implemented as a recursive 
+-- interpreter.
 eval' :: LamM m => (Exp -> m V) -> Exp -> m V
 eval' _ lam@(Lam _ _) =
    clo . (,lam) <$> getEnv
@@ -67,11 +91,18 @@ eval' recur (App e1 e2) = do
 eval' _ (Var (Ident nam _)) =
    lookupEnv nam >>= lookupAdr
 
+-- | Function application semantics
 apply :: LamM m => (Exp -> m V) -> Clo Adr -> V -> m V
 apply recur (env, Lam (Ident nam _) exp') v =
    withEnv (const env) $
       withExtendedEnv [(nam, Adr nam)] $ do
          writeAdr (Adr nam) v
+         -- the "magic" for having a terminating
+         -- analysis occurs here. Instead of calling 
+         -- `eval' recur` we call `recur` signaling
+         -- to the framework that the expression `exp'`
+         -- should be evaluated in a new fixpoint
+         -- computation.
          recur exp'
 apply _ _ _ = error "not a valid closure"
 
@@ -80,8 +111,11 @@ apply _ _ _ = error "not a valid closure"
 -- Analysis instantiation
 ------------------------------------------------------------
 
+-- | Shorthand for defining components in the analysis
 type Cmp = Key (IntraT Identity) Exp
+-- | Shorthand for defining results of the analysis
 type Res = Val (IntraT Identity) V
+-- | Monadic stack used in the intra analysis
 type IntraT m = MonadStack '[
       MayEscapeT (Set DomainError),
       EnvT (Map String Adr),
@@ -89,6 +123,9 @@ type IntraT m = MonadStack '[
       CacheT
    ] m
 
+-- | Constraints remaining for the inter-analysis
+-- and which are global to the fixpoint computation
+-- of the intra analysis.
 type InterM m = 
    (MapM Cmp Res m,
     WorkListM m Cmp, 
@@ -97,17 +134,39 @@ type InterM m =
     ComponentTrackingM m Cmp,
     StoreM m Adr V)
 
+-- | The intra analysis uses `runFixT` which automatically
+-- inserts the monad transformers defined in `IntraT` and 
+-- instantiates the open recursion with an appropriate 
+-- `recur` function.
+--
+-- `runIntraAnalysis` adds some additional bookkeeping 
+-- for scheduling the intra-analysis of components 
+-- in the fixed point computation.
 intra :: forall m . InterM m => Cmp -> m ()
 intra cmp = runFixT @(IntraT (IntraAnalysisT Cmp m)) eval cmp
           & runIntraAnalysis cmp
 
+-- | The `inter` analysis simply computes the 
+-- `intra` analysis for each component discovered
+-- during the analysis until a fixpoint is reached.
 inter :: forall m . InterM m => Exp -> m ()
 inter e = lfp intra (e, Map.empty)
 
+-- | This function calls the `inter` analysis 
+-- and instantiates the remaining monad type class
+-- constraints using additional monad transformers.
 analyze :: Exp -> (Map Adr V, Map Cmp Res)
 analyze prg = let (((), sto), res) = inter prg
                      & runWithStore @(Map Adr V)
                      & runWithMapping @Cmp @Res
+                     -- | The tracking transformers
+                     -- introduced here are needed 
+                     -- for the effect-driven fixed 
+                     -- point computation, if another 
+                     -- fixed point computation would 
+                     -- have been used, another set 
+                     -- of bookeeping transforms 
+                     -- would be here.
                      & runWithComponentTracking
                      & runWithDependencyTracking @Cmp @Cmp
                      & runWithDependencyTracking @Cmp @Adr
