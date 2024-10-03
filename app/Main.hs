91a69ce384f731e60f075f9a811a54ae34f77701
Bram Vandenbogaerde <bram@vdb.space>
2024-10-03 12:53:07 +0200

    feat: Added command line parsing
    enables switching between analyzers and interpreter
    enables specifying which file needs to be analyzed

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,52 @@
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NamedFieldPuns #-}
 module Main (main) where
 
 import Syntax.Compiler
-import Data.Either
 import Data.Map (Map)
 import Data.List (intercalate)
 import Text.Printf
 import qualified Data.Map as Map
-import Syntax.Span
+import Syntax.Span hiding (filename)
 import Syntax.Simplifier
 import Domain.Scheme.Store
 import Analysis.SimpleActor
+import Options.Applicative
+import Syntax.AST hiding (filename)
+import Control.Monad ((>=>))
+import Interpreter hiding (PrmAdr)
+import GHC.Base (join)
+
+
+------------------------------------------------------------
+-- Command-line arguments
+------------------------------------------------------------
+
+type Command = IO ()
+
+
+newtype InputOptions = InputOptions {
+                  filename :: String
+              } deriving (Show, Ord, Eq)
+
+
+inputOptions :: Parser InputOptions
+inputOptions = InputOptions <$> strOption
+                  (   long "file"
+                   <> short 'f'
+                   <> help "Input file"
+                  )
+
+commandParser :: Parser Command
+commandParser =
+   subparser
+    (   command "analyze" (info (analyzeCmd <$> inputOptions) (progDesc "Analyze a program"))
+    <>  command "eval"    (info (interpret <$> inputOptions) (progDesc "Run a program")))
+
+
+------------------------------------------------------------
+-- Inspecting analysis results
+------------------------------------------------------------
 
 printSto :: (Show v) => (k -> String) -> (k -> Bool) -> Map k v -> String
 printSto printKey keepKey m  =
@@ -21,15 +57,33 @@ printSto printKey keepKey m  =
 printLoc :: ActorCmp -> String
 printLoc (((((e, _), _), _), _), _) = let (Span filename line col) = spanOf e in show line ++ ":" ++ show col ++ "@" ++ filename
 
-main :: IO ()
-main = do
-   ast <- fmap (either (error . ("error while parsing: " ++)) id . parseFromString) (translate =<< readFile "test.scm")
-   print ast
+------------------------------------------------------------
+-- Entrypoints
+------------------------------------------------------------
+
+loadFile :: String -> IO Exp
+loadFile = readFile >=> translate >=> return . either (error . ("error while parsing: " ++)) id . parseFromString
+
+
+analyzeCmd :: InputOptions -> IO ()
+analyzeCmd (InputOptions { filename  }) = do 
+   ast <- loadFile filename
    let ((((), sto), mbs), res) = analyze ast
    putStrLn $ printSto show (\case { (PrmAdr _) -> False ; _ -> True }) sto
    putStrLn "====="
    putStrLn $ printSto printLoc (const True) res
    putStrLn "====="
    putStrLn $ printSto  show (const True) mbs
-   --runEval (eval ast) >>= print
    return ()
+
+interpret :: InputOptions -> IO ()
+interpret (InputOptions { filename }) = 
+   loadFile filename >>= runEval . eval >>= print
+
+------------------------------------------------------------
+-- Main entrypoint
+------------------------------------------------------------
+
+main :: IO ()
+main = join $ customExecParser p (info (commandParser <**> helper) fullDesc)
+   where p = prefs showHelpOnError
