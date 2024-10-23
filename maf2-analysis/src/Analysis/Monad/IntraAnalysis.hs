b1dc6091c69f545ebbd50eb0ceb01cfd9fae3f5d
Bram Vandenbogaerde <bram@vdb.space>
2024-10-23 08:14:42 +0200

    fix(analysis): implement `hasAdr` for IntraAnalysis

--- maf2-analysis/src/Analysis/Monad/IntraAnalysis.hs
+++ maf2-analysis/src/Analysis/Monad/IntraAnalysis.hs
@@ -15,7 +15,6 @@ import Analysis.Monad.DependencyTracking
 import Analysis.Monad.Store
 import Control.Monad.Cond
 import Analysis.Monad.Map
-import Debug.Trace
 
 ---
 --- IntraAnalysis monad layer 
@@ -35,6 +34,7 @@ instance {-# OVERLAPPING #-} (StoreM a v m, Eq v, DependencyTrackingM m cmp a, W
     writeAdr a v = whenM (upperM $ writeAdr' a v) (upperM $ trigger a)
     updateAdr a v = whenM (upperM $ updateAdr' a v) (upperM $ trigger a)
     updateWith fs fw a = whenM (upperM $ updateWith' fs fw a) (upperM $ trigger a)
+    hasAdr = upperM . hasAdr
 
 instance {-# OVERLAPPING #-} (MapM k v m, Eq v, DependencyTrackingM m cmp k, WorkListM m cmp)
     => MapM k v (IntraAnalysisT cmp m) where
