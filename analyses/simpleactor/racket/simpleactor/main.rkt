f745b2939b034cd00887fa909dd6e8a2ad865316
Bram Vandenbogaerde <bram@vdb.space>
2025-06-02 17:02:50 +0200

    feat(simpleactor): concrete actor language semantics in Racket

--- analyses/simpleactor/racket/simpleactor/main.rkt
+++ analyses/simpleactor/racket/simpleactor/main.rkt
@@ -8,6 +8,11 @@
 
 (module reader syntax/module-reader simpleactor)
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Instrumentation for soundness and precision testing
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ;; Concrete address of variables
 (struct vadr (var loc) #:transparent)
 
@@ -29,4 +34,39 @@
   (syntax-parse stx
     [(_ operator operands ...)
      #'(begin (displayln "app") (#%app operator operands ...))]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Actor language semantics
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(struct pid (tid exp) #:transparent)
+(define self (make-parameter #f))
+
+(define-syntax (spawn^ stx)
+  (syntax-parse stx
+    [(_ body)
+     #`(letrec
+          ((new-pid (pid (thread (lambda ()
+                                     (parameterize ([self (thread-receive)]) 
+                                       body))) (quote-syntax #,stx))))
+
+          (thread-send (pid-tid new-pid) new-pid)
+          new-pid)])) 
+
+(define-match-expander pair
+  (lambda (stx)
+    (syntax-case stx ()
+      [(_ v1 v2) #'(cons v1 v2)])))
+
+(define-syntax (receive stx)
+  (syntax-parse stx
+    [(_ ((pat body ...) ...))
+     #'(match (thread-receive)
+         [pat body ...] ...)]))
+
+(define (send^ ref vlu)
+  (thread-send (pid-tid ref) vlu))
+
+(define (wait-until-terminated ref)
+  (thread-wait (pid-tid ref)))
 
