23e32e28630eaf8ac2b141e83e961391cefb6457
Bram Vandenbogaerde <bram@vdb.space>
2025-06-02 16:35:41 +0200

    feat(simpleactor): wip concrete interpreter in Racket for simpleactor

--- analyses/simpleactor/racket/simpleactor/main.rkt
+++ analyses/simpleactor/racket/simpleactor/main.rkt
@@ -1,11 +1,32 @@
 #lang racket
 
-(provide (all-defined-out))
+(provide (all-defined-out)
+         (except-out (all-from-out racket) #%app)
+         (rename-out (app-instrumented #%app)))
+
 (require (for-syntax syntax/parse))
 
-(define-syntax (begin-program stx)
-  (syntax-parse stx
-    [(_ body ...)
-     #'(begin body ...)]))
+(module reader syntax/module-reader simpleactor)
 
-(provide (rename-out [begin-program #%module-begin]))
+;; Concrete address of variables
+(struct vadr (var loc) #:transparent)
+
+;; Concrete address of pointers (i.e., to strings, vectors, pairs, ...)
+(struct padr (exp loc) #:transparent)
+
+;; Shadow store
+(struct store (sto-storage) #:transparent)
+
+;; Shadow store
+(define *store* (store (make-hash)))
+
+;; Shadow stack
+(define *stack* (list))
+
+(define-for-syntax shadow-stack (gensym))
+
+(define-syntax (app-instrumented stx)
+  (syntax-parse stx
+    [(_ operator operands ...)
+     #'(begin (displayln "app") (#%app operator operands ...))]))
+
