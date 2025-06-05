9f0963bf5d338291b610167ed685849b58d8db23
sverbele <sverbele@soft.vub.ac.be>
2025-06-05 14:46:54 +0200

    feat(Python): WIP Python.Monad.Characteristics

--- maf2-analysis/src/Analysis/Monad/FunctionCharacteristics.hs
+++ maf2-analysis/src/Analysis/Monad/FunctionCharacteristics.hs
@@ -3,32 +3,12 @@
 {-# LANGUAGE RecordWildCards #-}
 module Analysis.Monad.FunctionCharacteristics where 
 
-
-import Lattice 
-import Control.Monad.State ( StateT, MonadState )
-import qualified Control.Monad.State as State 
-import Control.Monad.Trans.State (runStateT)
-import Control.Monad.Layer (MonadLayer (..))
-import Control.Monad.Trans (MonadTrans)
-import Control.Monad.Join ( MonadJoinable )
-import qualified Data.Map as Map
 import qualified Data.Set as Set
-import Domain (NumberDomain(plus))
-import Control.Monad.AbstractM (AbstractM)
 
 import Analysis.Monad.Map ( MapM(put, get) )
 import Data.Maybe (fromJust)
 import Syntax.Python (PyLoc)
 import Analysis.Python.Common (ObjAddrSet, emptyObjAddrSet)
-
-data Characteristic = CSNum      -- number of call sites that invoke the function
-                    | EquivCSNum -- number of equivalence classes of call sites that invoke the function
-                    | AllUse     -- number of uses of the this object and all parameters in the function
-                    | RCNum      -- number of abstract receiver objects from all call sites that invoke the function
-                    | ThisUse    -- number of uses of the this object in the function 
-                    | Par1Num    -- the total number of abstract objects to which the 1st parameter may point from all call sites that invoke the function
-                    | Par1Use    -- the total number of uses of the 1st parameter of a function (merge of FC7 and FC8 of the paper)
-    deriving (Eq, Ord, Show)
     
 data CharacteristicsMap = CharacteristicsMap { callSites :: Set.Set PyLoc, -- number of call sites that invoke the function
                                                equivCallSites :: Set.Set PyLoc, -- number of equivalence classes of call sites that invoke the function
