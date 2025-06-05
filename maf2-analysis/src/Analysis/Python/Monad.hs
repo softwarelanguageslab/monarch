8fd7eaee7fa07bb015cb8ea6d0ffbfc7e19d008a
sverbele <sverbele@soft.vub.ac.be>
2025-06-05 14:27:21 +0200

    feat(Python): PyRef->PyRefTaint and place PyM taint instance in separate file

--- maf2-analysis/src/Analysis/Python/Monad.hs
+++ maf2-analysis/src/Analysis/Python/Monad.hs
@@ -1,7 +1,6 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE ConstraintKinds #-}
-{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE AllowAmbiguousTypes #-}
 
 module Analysis.Python.Monad (
@@ -13,48 +12,27 @@ module Analysis.Python.Monad (
   pyDeref2'',
   pyDeref3',
   pyStore,
-  PyRef,
-  PyRet,
-  -- PyRefTaint,
-  -- PyRetTaint,
-  Taint,
-  runPythonTaintAnalysisT,
-  PythonTaintAnalysisT
+  -- PyRef,
+  -- PyRet,
 ) where
 
 import Lattice
-import Domain.Class
 import Control.Monad.Join
-import Control.Monad.DomainError
 import Control.Monad.Layer (MonadLayer (..))
 import Control.Monad.Escape
 import Analysis.Python.Common
 import Analysis.Monad hiding (call, get)
 import Domain.Python.Syntax hiding (Continue, Break, Return, None)
 import Domain.Python.World
-import Analysis.Python.Escape
 
 import Prelude hiding (lookup, exp, break)
 import Control.Monad.AbstractM (AbstractM)
-import Control.Monad ((>=>), (<=<))
-import Analysis.Monad.Call (CallM(..))
-import Lattice.Tainted (Tainted(..))
+import Control.Monad ((<=<))
 import Data.Singletons (SingI, Sing)
-import Data.Set (Set)
-import Data.Functor (($>))
-import qualified Data.Set as Set
-import qualified Lattice.TopLiftedLattice as TopLattice
 import Text.Printf (printf)
 import Lattice.TopLattice()
 import Domain.Python.Objects.Class
 import Control.Monad.Identity
-
-
-newtype PythonTaintAnalysisT m a = PythonTaintAnalysisT (IdentityT m a)
-  deriving (Functor, Applicative, Monad, MonadJoinable, MonadLayer, MonadEscape, MonadCache)
-
-runPythonTaintAnalysisT :: PythonTaintAnalysisT m a -> m a
-runPythonTaintAnalysisT (PythonTaintAnalysisT m) = runIdentityT m
 
 newtype PythonAnalysisT m a = PythonAnalysisT (IdentityT m a)
   deriving (Functor, Applicative, Monad, MonadJoinable, MonadLayer, MonadEscape, MonadCache)
@@ -131,80 +109,6 @@ pyStore loc = fmap injectAdr . pyAlloc loc
 
 
 
--- Taint analysis instance 
-
--- kcfa k
--- TODO: parameterize this
-kcfa :: Int
-kcfa = 10
-
--- type Taint = TopLifted (Set String)
--- type PyRefTaint = Tainted Taint ObjAddrSet
--- type PyRetTaint = Tainted Taint (Set (PyEsc PyRefTaint))
-
-type Taint = TopLifted (Set String)
-type PyRef = Tainted Taint ObjAddrSet
-type PyRet = Tainted Taint (Set (PyEsc PyRef))
-
-instance (
-          vlu ~ PyRef,
-          -- vlu ~ PyRefTaint,
-          PyDomain obj vlu,
-          TaintM Taint m,
-          CtxM m [PyLoc],
-          MonadJoin m,
-          MonadEscape m,
-          CallM (PyLoc, PyBdy) PyRef m,
-          -- CallM (PyLoc, PyBdy) PyRefTaint m,
-          PyEscape (Esc m) vlu,
-          Domain (Esc m) PyError,
-          Domain (Esc m) DomainError,
-          BottomLattice (Esc m),
-          SplitLattice (Esc m),
-          EnvM m ObjAdr PyEnv,
-          AllocM m PyLoc ObjAdr,
-          GraphM (CP String) (CP Bool) m,
-          StoreM ObjAdr obj m)
-          =>
-          PyM (PythonTaintAnalysisT m) obj vlu where
-  pyStoreSize = storeSize @ObjAdr @obj @(PythonTaintAnalysisT m)
-  pyCall = curry call
-  pyAlloc = store
-  pyDeref f = unwrapTainted (deref f . addrs)
-  pyDeref_ f = unwrapTainted_ (deref f . addrs)
-  pyAssignAt k v a = addTaint v >>= \v' -> updateWith (setAttr k v') (setAttrWeak k v') a
-  pyAssign k v = unwrapTainted_ (mjoinMap (pyAssignAt k v) . addrs) --- !!! important
-  pyAssignInPrm s f v = pyDeref_ $ \adr obj -> do v' <- addTaint v
-                                                  old <- getPrm s obj
-                                                  upd <- f v' old
-                                                  let obj' = setPrm s upd obj
-                                                  updateAdr adr obj'
-  pyBreak = escape (Break @vlu)
-  pyContinue = escape (Continue @vlu)
-  pyReturn = escape . Return
-  pyError = escape
-  pyRaise = escape . Exception
-  pyCatchExc bdy hdl = bdy `catchOn` (isException, getException >=> hdl)
-  pyCatchLoop bdy brk cnt = bdy `catchOn` (isBreak, const brk) `catchOn` (isContinue, const cnt)
-  pyReturnable = (`catchOn` (isReturn, getReturn))
-  pyGetEnv = getEnv
-  pyWithEnv = withEnv . const
-  pyLookupEnv = lookupEnv
-  pyLookupSto = lookupAdr
-  pyWithCtx loc = withCtx (take kcfa . (loc:))
-  applyXPrim ObjectTaint _ = \case
-                                [a] -> withTaint @Taint TopLattice.Top (addTaint a)
-                                _   -> pyError ArityError
-  applyXPrim DatabaseRead loc = \case
-                                  [_, str] -> pyDeref'' @StrPrm (\nam -> withTaint @Taint (toTaint nam) $ addTaint =<< pyStore loc (from' @DfrPrm Prelude.False)) str
-                                  _        -> pyError ArityError
-                                  where toTaint (Lattice.Constant str) = TopLattice.Value (Set.singleton str)
-                                        toTaint Lattice.Top            = TopLattice.Top
-  applyXPrim DatabaseWrite _ = \case
-                                  [_, df, str] -> pyDeref2'' @DfrPrm @StrPrm (\dfr nam -> currentTaint @Taint >>= \t -> addEdges nam t dfr $> constant None) df str
-                                  _            -> pyError ArityError
-                                  where addEdges to TopLattice.Top dfr = addEdge Lattice.Top to dfr
-                                        addEdges to (TopLattice.Value s) dfr = mapM_ (\source -> addEdge (Lattice.Constant source) to dfr) (Set.toList s)
 
 --- canonical instance
 
---
+++ maf2-analysis/src/Analysis/Python/Monad/Taint.hs
@@ -0,0 +1,105 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
+module Analysis.Python.Monad.Taint where 
+
+import Lattice
+import Domain.Class
+import Control.Monad.Join
+import Control.Monad.Layer (MonadLayer (..))
+import Control.Monad.DomainError
+import Control.Monad.Escape
+import Analysis.Python.Common
+import Analysis.Monad hiding (call, get)
+import Domain.Python.Syntax hiding (Continue, Break, Return, None)
+import Domain.Python.World
+import Analysis.Python.Escape
+import Analysis.Python.Monad
+
+import Prelude hiding (lookup, exp, break)
+import Control.Monad ((>=>), (<=<))
+import Analysis.Monad.Call (CallM(..))
+import Lattice.Tainted (Tainted(..))
+import Data.Set (Set)
+import Data.Functor (($>))
+import qualified Data.Set as Set
+import qualified Lattice.TopLiftedLattice as TopLattice
+import Lattice.TopLattice()
+import Domain.Python.Objects.Class
+import Control.Monad.Identity
+
+newtype PythonTaintAnalysisT m a = PythonTaintAnalysisT (IdentityT m a)
+  deriving (Functor, Applicative, Monad, MonadJoinable, MonadLayer, MonadEscape, MonadCache)
+
+runPythonTaintAnalysisT :: PythonTaintAnalysisT m a -> m a
+runPythonTaintAnalysisT (PythonTaintAnalysisT m) = runIdentityT m
+
+
+-- Taint analysis instance
+
+-- kcfa k
+-- TODO: parameterize this
+kcfa :: Int
+kcfa = 10
+
+type Taint = TopLifted (Set String)
+type PyRefTaint = Tainted Taint ObjAddrSet
+type PyRetTaint = Tainted Taint (Set (PyEsc PyRefTaint))
+
+instance (
+          vlu ~ PyRefTaint,
+          PyDomain obj vlu,
+          TaintM Taint m,
+          CtxM m [PyLoc],
+          MonadJoin m,
+          MonadEscape m,
+          CallM (PyLoc, PyBdy) PyRefTaint m,
+          PyEscape (Esc m) vlu,
+          Domain (Esc m) PyError,
+          Domain (Esc m) DomainError,
+          BottomLattice (Esc m),
+          SplitLattice (Esc m),
+          EnvM m ObjAdr PyEnv,
+          AllocM m PyLoc ObjAdr,
+          GraphM (CP String) (CP Bool) m,
+          StoreM ObjAdr obj m)
+          =>
+          PyM (PythonTaintAnalysisT m) obj vlu where
+  pyStoreSize = storeSize @ObjAdr @obj @(PythonTaintAnalysisT m)
+  pyCall = curry call
+  pyAlloc = store
+  pyDeref f = unwrapTainted (deref f . addrs)
+  pyDeref_ f = unwrapTainted_ (deref f . addrs)
+  pyAssignAt k v a = addTaint v >>= \v' -> updateWith (setAttr k v') (setAttrWeak k v') a
+  pyAssign k v = unwrapTainted_ (mjoinMap (pyAssignAt k v) . addrs) --- !!! important
+  pyAssignInPrm s f v = pyDeref_ $ \adr obj -> do v' <- addTaint v
+                                                  old <- getPrm s obj
+                                                  upd <- f v' old
+                                                  let obj' = setPrm s upd obj
+                                                  updateAdr adr obj'
+  pyBreak = escape (Break @vlu)
+  pyContinue = escape (Continue @vlu)
+  pyReturn = escape . Return
+  pyError = escape
+  pyRaise = escape . Exception
+  pyCatchExc bdy hdl = bdy `catchOn` (isException, getException >=> hdl)
+  pyCatchLoop bdy brk cnt = bdy `catchOn` (isBreak, const brk) `catchOn` (isContinue, const cnt)
+  pyReturnable = (`catchOn` (isReturn, getReturn))
+  pyGetEnv = getEnv
+  pyWithEnv = withEnv . const
+  pyLookupEnv = lookupEnv
+  pyLookupSto = lookupAdr
+  pyWithCtx loc = withCtx (take kcfa . (loc:))
+  applyXPrim ObjectTaint _ = \case
+                                [a] -> withTaint @Taint TopLattice.Top (addTaint a)
+                                _   -> pyError ArityError
+  applyXPrim DatabaseRead loc = \case
+                                  [_, str] -> pyDeref'' @StrPrm (\nam -> withTaint @Taint (toTaint nam) $ addTaint =<< pyStore loc (from' @DfrPrm Prelude.False)) str
+                                  _        -> pyError ArityError
+                                  where toTaint (Lattice.Constant str) = TopLattice.Value (Set.singleton str)
+                                        toTaint Lattice.Top            = TopLattice.Top
+  applyXPrim DatabaseWrite _ = \case
+                                  [_, df, str] -> pyDeref2'' @DfrPrm @StrPrm (\dfr nam -> currentTaint @Taint >>= \t -> addEdges nam t dfr $> constant None) df str
+                                  _            -> pyError ArityError
+                                  where addEdges to TopLattice.Top dfr = addEdge Lattice.Top to dfr
+                                        addEdges to (TopLattice.Value s) dfr = mapM_ (\source -> addEdge (Lattice.Constant source) to dfr) (Set.toList s)
