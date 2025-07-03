518482c62e60b9ce13a8b244c64c07f8af30e76c
Bram Vandenbogaerde <bram@vdb.space>
2025-07-03 15:46:58 +0200

    fix(simpleactor): fix count savina benchmark

--- analyses/simpleactor/programs/preprocessed/savina_regular_extra/original/fjc-seq7.scm
+++
@@ -1,8 +0,0 @@
-(letrec ((N 3)
-         (perform-computation
-          (lambda (theta) (let ((sint (+ 1 theta))) (* sint sint))))
-         (forkjoin-actor
-          (behavior () ((message () (perform-computation 37.2) (terminate))))))
-  '()
-  '()
-  '())
