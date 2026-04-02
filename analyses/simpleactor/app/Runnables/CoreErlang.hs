284fd0cd7e93cf4022e0a69f74ec068539e080f0
Bram Vandenbogaerde <bram@vdb.space>
2026-04-02 15:11:58 +0200

    feat(simpleactor): multi-module Core Erlang module loader

--- analyses/simpleactor/app/Runnables/CoreErlang.hs
+++ analyses/simpleactor/app/Runnables/CoreErlang.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE PackageImports #-}
 module Runnables.CoreErlang
   ( options,
     entrypoint,
@@ -12,7 +13,6 @@ import CommandLine.Options
 import CommandLine.Options qualified as Options
 import CommandLine.Output qualified as Output
 import Control.Exception
-import Control.Monad (join, void)
 import Control.Monad.Error.Class
 import Control.Monad.Except
 import Control.Monad.IO.Class
@@ -28,6 +28,9 @@ import Syntax.SimpleActor.CoreToSimpleActor qualified as CoreToSimpleActor
 import qualified Domain.Core.BoolDomain.Class as Domain
 import System.Posix.Signals
 import Control.Concurrent
+import qualified System.FilePath.Glob as Glob
+import System.Directory (doesDirectoryExist)
+import "extra" Control.Monad.Extra
 
 ---------------------------------------------------------------------
 -- Command-line options
@@ -54,41 +57,54 @@ options =
 -- Entrypoint
 ---------------------------------------------------------------------
 
--- | Run the analyiss on a Core Erlang program described by the given options
+-- | Recursively enumerate the contents of the directory in order to
+-- find Erlang core files (*.core)
+directoryContents :: String -- ^ the path to the directory
+                  -> IO [String]
+directoryContents = Glob.globDir1 (Glob.compile "**/*.core")
+
+-- | Run the analyis on a Core Erlang program described by the given options
 analyzeOptions :: (MonadIO m, MonadError OutputAnalysisError m) => CoreErlangOptions -> m OutputAnalysisResult
 analyzeOptions CoreErlangOptions {..} = do
-  -- TODO: support more than one module, perhaps based on a directory
-  -- of modules.
-  let inputName = filename coreErlangInputOptions
-  contents <- liftIO $ readFile inputName
-  case CoreErlang.parseProgram inputName contents of
-    Left err -> do
-      liftIO $ putStrLn "Parse error:"
-      throwError $ ParseError $ show $ CoreErlang.prettyErrorBundle err
-    Right coreModule -> do
-      let expr = CoreToSimpleActor.translateModules [coreModule] coreErlangMainMod coreErlangMainFun
+        -- Retrieve the path to the input, can be a file or directory
+        let inputName = filename coreErlangInputOptions
+        -- If the input is a directory then load all Core Erlang modules
+        -- from that directory otherwise use the filename as the first and only module.
+        files <- liftIO $ ifM (doesDirectoryExist inputName)
+                              (directoryContents inputName)
+                              (pure [inputName])
+        -- Parse the Core Erlang code in the Core Erlang AST
+        parsedFiles <- mapM (\filename -> liftIO (readFile filename) >>= parseProgram filename) files
+        -- Translate Core Erlang into the SimpleActor language, the "coreErlangMainMod" and
+        -- "coreErlangMainFun" originate from the "CoreErlangOptions" and are passed by the user
+        -- as command line arguments.
+        let expr = CoreToSimpleActor.translateModules parsedFiles coreErlangMainMod coreErlangMainFun
+        -- Collect all soter predicates ... 
+        let predicates = foldMap (Soter.fromAttributes . CoreErlang.unAnn) parsedFiles
+        -- ... and find suitable bounds for each variable in the predicates:
+        let bounds = Map.fromList $ map (bimap MailboxLabel Count.zero) $ Map.toList $ Soter.extractBoundsPredicates predicates
 
-      -- Initialize soter predicates
-      let predicates = Soter.fromAttributes (CoreErlang.unAnn coreModule)
-      let bounds = Map.fromList $ map (bimap MailboxLabel Count.zero) $ Map.toList $ Soter.extractBoundsPredicates predicates
+        -- Run analysis
+        (AnalysisResult res _ countMax) <- liftIO $ analyze' bounds Nothing expr
 
-      -- Run analysis
-      (AnalysisResult res _ countMax) <- liftIO $ analyze' bounds Nothing expr
+        -- Compute soter results
+        let countOutMapping =
+              Map.mapKeys getMailboxLabel $ Map.mapMaybe (\case Count.Count v _ -> Just v; _ -> Nothing) countMax
+        let predicateHolds = map (Domain.isTrue . Soter.predicateHolds countOutMapping) predicates
 
-      -- Compute soter results
-      let countOutMapping =
-            Map.mapKeys getMailboxLabel $ Map.mapMaybe (\case Count.Count v _ -> Just v; _ -> Nothing) countMax
-      let predicateHolds = map (Domain.isTrue . Soter.predicateHolds countOutMapping) predicates
+        liftIO $ print countOutMapping
+        liftIO $ print predicateHolds
+        liftIO $ print (sum $ map (length . Map.toList . Sequential.cmpRes .  snd) $ Map.toList res)
 
-      liftIO $ print countOutMapping
-      liftIO $ print predicateHolds
-      liftIO $ print (sum $ map (length . Map.toList . Sequential.cmpRes .  snd) $ Map.toList res)
+        return $
+          AnalysisOutput
+            { reachableCoverableConditions = sum (map fromEnum predicateHolds),
+              totalPredicates = length predicates
+            }
 
-      return $
-        AnalysisOutput
-          { reachableCoverableConditions = sum (map fromEnum predicateHolds),
-            totalPredicates = length predicates
-          }
+  where parseProgram filename =
+           -- parse the contents and lift a potential error in a suitable error type
+           liftEither . first (ParseError . show . CoreErlang.prettyErrorBundle) . CoreErlang.parseProgram filename
 
 -- | Set of signals we are interested in for recovering from particular set of resource limits.
 --
