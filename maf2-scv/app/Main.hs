e7853a4657fbbe38f765c8e36128dab5e54f84f3
Bram Vandenbogaerde <bram@vdb.space>
2026-04-13 16:04:55 +0200

    chore(scv/cleanup): remove unused imports

--- maf2-scv/app/Main.hs
+++ maf2-scv/app/Main.hs
@@ -3,24 +3,9 @@ module Main (main) where
 import Solver
 import Symbolic.AST
 import Symbolic.SMT
-import Syntax.Scheme
-import qualified Syntax.Scheme.Actor as Actor
 
 import qualified Data.Map as Map
-import Data.Map (Map)
-import Data.List ( intercalate )
-import Text.Printf
 
-import System.Environment
-import System.Process
-import System.IO
-import Syntax.Scheme.Parser (parseDatum)
-import Data.Either (fromRight)
-import qualified Syntax.Scheme.Parser as SExp
-import Control.Monad.State
-import Data.Functor (($>))
-import Debug.Trace (traceShowId)
-import qualified Data.Sequence as SExp
 
 exampleFormula :: (Formula String)
 exampleFormula = conjunction
