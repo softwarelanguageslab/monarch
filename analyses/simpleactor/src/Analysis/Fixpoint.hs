758f71a29445b27567e7b94c5bb59937f9559043
Bram Vandenbogaerde <bram@vdb.space>
2026-06-10 09:39:42 +0200

    debugging(simpleactor): add iterations counts

--- analyses/simpleactor/src/Analysis/Fixpoint.hs
+++ analyses/simpleactor/src/Analysis/Fixpoint.hs
@@ -2,8 +2,10 @@
 module Analysis.Fixpoint(lfp, lfpTraced, lift) where
 
 import Data.Set (Set)
-import RIO (foldMapM)
+import RIO (foldMapM, hFlush, stdout)
 import qualified Data.Set as Set
+import Control.Monad.IO.Class
+import qualified Debug.Trace as Debug
 
 -- | Naive equality based fixpoint computation
 lfp :: (Monad m, Eq a) => (a -> m a) -> a -> m a
@@ -22,6 +24,10 @@ lfpTraced f = flip go []
           if next /= prev' then go next (prev' : acc) else return (prev', acc)
 
 -- | Lift a function 'a -> Set a' to 'Set a -> Set a'.
-lift :: (Monad m, Ord a) => (a -> m (Set a)) -> Set a -> m (Set a)
-lift f s = Set.union s <$> foldMapM f s
+lift :: (MonadIO m, Ord a) => (a -> m (Set a)) -> Set a -> m (Set a)
+lift f s = do 
+    liftIO (Debug.traceIO $ "LIFT-PRE: set size in " ++  show (Set.size s))
+    result <- Set.union s <$> foldMapM (\(v, i) -> liftIO (putStr (show i ++ " ") >> hFlush stdout) >> f v) (Set.fromList $ zip (Set.toList s) [(0 :: Int)..])
+    liftIO (Debug.traceIO $ "LIFT: set size in " ++  show (Set.size s) ++ " set size out " ++ show (Set.size result))
+    return result
 
